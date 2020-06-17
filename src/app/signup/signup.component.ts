c77c84e9c7fb030bc83723c94711823252f1a4ee
EngineerSk <eskayoriade@gmail.com>
2020-06-17 15:57:31 +0100

    created signup component

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '.app-signup',
+  template: `<h2>Hello</h2>
+              <p>This is a Sign up page</p>`,
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
